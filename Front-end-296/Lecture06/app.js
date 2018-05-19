aadf3460fca15fdd67d81b5f96979264df6bc0d4
resnick1223 <resnick1223@gmail.com>
2018-05-19 12:13:31 +0800

    新增296第7講內容

--- Front-end-296/Lecture06/app.js
+++ Front-end-296/Lecture06/app.js
@@ -1,4 +1,59 @@
-var cart = [];
+var provider = new firebase.auth.GoogleAuthProvider();
+var uid;
+$("#login").click(function () {
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+});
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        uid = user.uid;
+        var providerData = user.providerData;
+        $("#username").text(displayName);
+
+        // 如果使用者登入成功
+        // 顯示 登出按鈕 隱藏 登入按鈕
+        $("#user-photo").attr("src", photoURL);
+        $("#login").addClass("hidden");
+        $("#logout").removeClass("hidden");
+        $("#checkout").removeClass("hidden");
+    } else {
+        // 如果使用者沒有登入
+        // 顯示 登入按鈕 隱藏 登出按鈕
+        $("#login").removeClass("hidden");
+        $("#logout").addClass("hidden");
+        $("#checkout").addClass("hidden");
+
+    }
+});
+
+if (localStorage["cart"] === undefined) {
+    var cart = {};
+} else {
+    cart = JSON.parse(localStorage["cart"]);
+    render();
+}
 
 // 1.取得一個產品的文件
 // 1.1 建立一個文件的參考
@@ -36,10 +91,16 @@ productsRef.get().then(function (snapshot) {
                     var selectedProduct = doc.data();
                     console.log(selectedProduct);
                     // 把資料加入購物車
-                    cart.push({
-                        product: selectedProduct,
-                        count: 1
-                    });
+                    if (cart[productId] === undefined) {
+                        cart[productId] = {
+                            product: selectedProduct,
+                            count: 1
+                        };
+                    } else {
+                        cart[productId].count++;
+                    }
+
+                    localStorage['cart'] = JSON.stringify(cart);
                     // 繪製表格
                     render();
 
@@ -55,7 +116,8 @@ function render() {
     //var $row = $("")
     $("#order-content").html("");
     var total = 0;
-    cart.forEach(function (record) {
+    Object.keys(cart).forEach(function (key) {
+        var record = cart[key];
         var $row = $("<tr>");
         var html = `
         <td>${record.product.name} - ${record.product.storage}GB</td>
@@ -68,3 +130,13 @@ function render() {
     });
     $("#total").text(`${total}元`);
 }
+
+$("#checkout").click(function () {
+    var order = cart;
+    order.uid = uid;
+    db.collection("orders").add(order);
+    alert("訂單已送出");
+    cart = {};
+    delete localStorage['cart'];
+    render();
+});
